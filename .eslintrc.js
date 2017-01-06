0c2f2b59fc9a569c0fa24e57675d607083df4115
tsukiy0 <tsukiy0@users.noreply.github.com>
2017-01-06 15:46:17 +1100

    Add linting script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
     "extends": "google",
-    "installedESLint": true
+    "installedESLint": true,
+    "rules": {
+        "require-jsdoc": 0,
+        "new-cap": 0,
+        "max-len": 1,
+        "no-multiple-empty-lines": [
+            "error",
+            {"max": 1}
+        ]
+    },
 };
